fa4d695379d2f9a140694d87073163c05ad1a67f
Frank Lan <franklan118@gmail.com>
2018-05-20 17:25:11 +1000

    include font awesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './styles.sass'
+import 'font-awesome/css/font-awesome.css'
 
 const reactContainerId = 'react-root'
 
